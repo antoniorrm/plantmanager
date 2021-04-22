d76ae858da5819e3af0a35df11f308aa28d42529
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-04-22 20:20:59 -0300

    add fonts and icons and updates

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,64 +1,83 @@
 import React from "react";
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from "react-native";
+
+import { Feather } from "@expo/vector-icons";
 
 import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-      Gerencie {'\n'}
-      suas plantas {'\n'}
-      de forma fácil</Text>
-      <Image source={wateringImg} style={styles.image}/>
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}> 
-          <Text style={styles.buttonText}>
-              >
-          </Text>
-      </TouchableOpacity>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de {"\n"}
+          forma fácil
+        </Text>
+
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "space-around",
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: "bold",
-        textAlign: "center",
-        color: colors.heading,
-        marginTop: 38,
-    },
-    subtitle: {
-        fontSize: 18,
-        paddingHorizontal: 20,
-        textAlign: "center",
-        color: colors.heading,
-    },
-    button: {
-        backgroundColor: colors.green,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        width: 56,
-    },
-    buttonText: {
-        color: colors.white,
-        fontSize: 24,
-    },
-    image: {
-        height: 292,
-        width: 292,
-    }
-})
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontFamily: fonts.text,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: colors.heading,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
+  },
+});
