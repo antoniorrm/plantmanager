212ec48eb361a18bda92642c81eb30cb5db625ec
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-04-22 20:20:07 -0300

    add fonts and app loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Welcome } from "./src/pages/Welcome";
+import { Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Welcome />;
 }
 
